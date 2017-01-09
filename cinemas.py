733bc4b50ce3b20802c41c8f0a946364cc1aebec
SBKubric <sbkubric@gmail.com>
2017-01-09 18:22:01 +0300

    Got cinemas counter

--- cinemas.py
+++ cinemas.py
@@ -15,7 +15,15 @@ def parse_afisha_list(raw_html):
     movie_titles_list = list(
         title_tag.findChild('a').text for title_tag in title_tag_list
     )
-    return movie_titles_list
+    schedule_tag = afisha_soup.find(attrs={'class': 'b-theme-schedule m-schedule-with-collapse'})  # type: BeautifulSoup
+    cinemas_schedule_soups = schedule_tag.find_all('table')
+    cinemas_counters = list(
+        len(cinemas_schedule_soup.findChild('tbody')) for cinemas_schedule_soup in cinemas_schedule_soups
+    )
+    return ({
+        'title': movie_title,
+        'cinema_counter': cinemas_counter,
+    } for movie_title, cinemas_counter in zip(movie_titles_list, cinemas_counters))
 
 
 def fetch_movie_info(movie_title):
@@ -28,6 +36,6 @@ def output_movies_to_console(movies):
 
 if __name__ == '__main__':
     raw_html = fetch_afisha_page()
-    movie_titles_list = parse_afisha_list(raw_html)
-    for num, movie_title in enumerate(movie_titles_list, start=1):
-        print('{}. {}'.format(num, movie_title))
+    movie_data_list = parse_afisha_list(raw_html)
+    for num, movie_data in enumerate(movie_data_list, start=1):
+        print('{}. Title: {} Cinemas: {}'.format(num, movie_data['title'], movie_data['cinema_counter']))
