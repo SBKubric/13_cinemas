e71e2d724f5d565179edc22e41653547699f2cad
SBKubric <sbkubric@gmail.com>
2017-01-09 17:43:52 +0300

    Got movies

--- cinemas.py
+++ cinemas.py
@@ -1,11 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+SCHEDULE_URL = 'http://www.afisha.ru/msk/schedule_cinema/'
 
 
 def fetch_afisha_page():
-    pass
+    afisha_page = requests.get(SCHEDULE_URL)
+    return afisha_page.content
 
 
 def parse_afisha_list(raw_html):
-    pass
+    afisha_soup = BeautifulSoup(raw_html, 'html.parser')
+    title_tag_list = afisha_soup.find_all(attrs={'class': 'theme-item__title '})
+    movie_titles_list = list(
+        title_tag.findChild('a').text for title_tag in title_tag_list
+    )
+    return movie_titles_list
 
 
 def fetch_movie_info(movie_title):
@@ -17,4 +27,7 @@ def output_movies_to_console(movies):
 
 
 if __name__ == '__main__':
-    pass
+    raw_html = fetch_afisha_page()
+    movie_titles_list = parse_afisha_list(raw_html)
+    for num, movie_title in enumerate(movie_titles_list, start=1):
+        print('{}. {}'.format(num, movie_title))
